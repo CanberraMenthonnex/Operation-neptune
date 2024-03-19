c81ce82dd2f404f15e4a861c244e7c7299cc10a4
Maxime Perraud <contact@maxime-p.dev>
2024-03-19 11:27:25 +0100

    fix: button

--- client/src/components/primitives/Button/Button.tsx
+++ client/src/components/primitives/Button/Button.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 
-import { ButtonProps } from './button.interface'
+import { ButtonProps } from './button.interface.ts'
 import styles from './button.module.css'
 
 function Button({ title, color }: ButtonProps) {
