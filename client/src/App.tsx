66b780ea1ca65f6800a339394f78bb5957f5192b
Maxime Perraud <contact@maxime-p.dev>
2024-03-19 16:27:51 +0100

    fix(client): removing components in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,7 @@
-import Input from './components/atoms/Input/Input'
-import TextArea from './components/atoms/TextArea/TextArea.tsx'
-import FormInput from './components/molecules/FormInput/FormInput'
-
 function App() {
   return (
     <>
       <h1>Hello world</h1>
-      <Input />
-      <FormInput label="Name" />
-      <TextArea label="Description" />
     </>
   )
 }
--- client/src/pages/README.md
+++ client/src/pages/README.md
@@ -1,3 +1,3 @@
 # Pages
 
-All pages are located in the `src/pages` directory. Each page is a React component that is rendered by the router. The router is located in `src/navvigation`.
+All pages are located in the `src/pages` directory. Each page is a React component that is rendered by the router. The router is located in `src/navigation`.
