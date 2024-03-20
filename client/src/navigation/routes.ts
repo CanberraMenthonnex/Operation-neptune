4c6f8606399ffb38fed60ee5fb9af582b9a6f706
Sverker <70761787+SveRKeR92@users.noreply.github.com>
2024-03-20 13:16:22 +0100

    feat: edit character page (#40)
    * feat: edit character page
    
    * feat: edit character page
    
    * delete empty file
    
    * feat(api): scenario generation
    
    * feat(client): adding router (#38)
    
    * feat: navbar component (#36)
    
    * feat: navbar component
    
    * fix(client): fix icons
    
    * fix(client): fix extension
    
    * fix(client): add icon
    
    * fix(client): user icon
    
    ---------
    
    Co-authored-by: Maxime Perraud <contact@maxime-p.dev>
    
    * feat(client): finishing navigation (#39)
    
    * feat(client): characters screen (#41)
    
    * feat: use Box component, update routes.ts
    
    ---------
    
    Co-authored-by: Mathieu <mathieu.raimbault@hotmail.com>
    Co-authored-by: Maxime  P <contact@maxime-p.dev>
    Co-authored-by: 09TT09 <55156826+09TT09@users.noreply.github.com>

--- client/src/navigation/routes.ts
+++ client/src/navigation/routes.ts
@@ -1,8 +1,10 @@
 import Characters from '../pages/Characters/Characters.tsx'
+import EditCharacter from '../pages/Characters/EditCharacter.tsx'
 import Home from '../pages/Home'
 import { Routes } from './routes.interface'
 
 export const routes: Routes[] = [
   { Page: Home, path: '/' },
   { Page: Characters, path: '/characters' },
+  { Page: EditCharacter, path: '/characters/edit' },
 ]
